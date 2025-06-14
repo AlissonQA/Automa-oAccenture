4fcbce1202d1195a94c30c8786727d260cb4751b
Alisson Santos de Andrade <alisson.andrade@keeggo.com>
2025-06-13 21:33:34 -0300

    ajuste linha de comando

--- src/test/java/RunnerTest.java
+++ src/test/java/RunnerTest.java
@@ -5,7 +5,7 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources/features", 
-		glue = "steps", 
+		glue = "Steps", 
 		plugin = { "pretty", "html:target/report.html" }, monochrome = true)
 public class RunnerTest {
 }
